5be505d7830b233d19f4b4d22eee3673c679d9e7
Erich <hcire3@hotmail.com>
2019-04-07 19:56:15 -0400

    Initial commit - Project start

---
+++ ParkingLot.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParkingLotCodeTest.Models;
+using ParkingLotCodeTest.Controllers;
+
+namespace ParkingLot.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
